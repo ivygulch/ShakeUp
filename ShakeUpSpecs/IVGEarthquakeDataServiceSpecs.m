28de78975c388b4c4cea82627308608ccacf6d49
Douglas Sjoquist <dwsjoquist@sunetos.com>
2013-03-22 03:27:17 -0400

    add nested context to reuse sample data so we can add more specs to check for proper values

--- ShakeUpSpecs/IVGEarthquakeDataServiceSpecs.m
+++ ShakeUpSpecs/IVGEarthquakeDataServiceSpecs.m
@@ -39,7 +39,7 @@ describe(@"earthquakeDataService", ^{
 
             [[spy.argument should] equal:kIVGUSGSEarthquake7DayM1URI];
         });
-        
+
     });
 
     context(@"successBlock", ^{
@@ -60,23 +60,72 @@ describe(@"earthquakeDataService", ^{
             [[@(loadDataBlockCalled) should] equal:@(YES)];
         });
 
-        it(@"should create array of dictionaries with earthquake data built from network request results", ^{
-            __block NSArray *loadedData = nil;
-            IVGEDSLoadDataBlock dummyLoadDataBlock = ^(NSArray *data) {
-                loadedData = data;
-            };
-            NSString *exampleUSGSDataString = @""\
-            "Src,Eqid,Version,Datetime,Lat,Lon,Magnitude,Depth,NST,Region" \
-            "\nak,10679205,1,\"Friday, March 22, 2013 04:48:46 UTC\",60.4056,-150.9621,2.2,47.80, 7,\"Kenai Peninsula, Alaska\"" \
-            "\nnc,71958305,0,\"Friday, March 22, 2013 04:24:39 UTC\",35.5412,-120.7735,1.2,5.10,10,\"Central California\"";
-            NSData *exampleUSGSData = [exampleUSGSDataString dataUsingEncoding:NSUTF8StringEncoding];
+        context(@"with sample USGS data", ^{
+            __block NSData *exampleUSGSData;
+            __block NSArray *loadedData;
+            __block IVGEDSLoadDataBlock dummyLoadDataBlock;
+            __block NSDictionary *exampleDictionary1;
+            __block NSDictionary *exampleDictionary2;
 
-            successBlock = [earthquakeDataService buildSuccessBlockWithLoadDataBlock:dummyLoadDataBlock];
-            successBlock(nil,exampleUSGSData);
-            [[loadedData should] haveCountOf:2];
-            for (id item in loadedData) {
-                [[item should] beKindOfClass:[NSDictionary class]];
-            }
+            beforeEach(^{
+                NSString *exampleUSGSDataString = @""\
+                "Src,Eqid,Version,Datetime,Lat,Lon,Magnitude,Depth,NST,Region" \
+                "\nak,10679205,1,\"Friday, March 22, 2013 04:48:46 UTC\",60.4056,-150.9621,2.2,47.80, 7,\"Kenai Peninsula, Alaska\"" \
+                "\nnc,71958305,0,\"Friday, March 22, 2013 04:24:39 UTC\",35.5412,-120.7735,1.2,5.10,10,\"Central California\"";
+
+                exampleDictionary1 =
+                @{
+                  @"Src":@"ak",
+                  @"Eqid":@"10679205",
+                  @"Version":@"1",
+                  @"Datetime":@"Friday, March 22, 2013 04:48:46 UTC",
+                  @"Lat":@"60.4056",
+                  @"Lon":@"-150.9621",
+                  @"Magnitude":@"2.2",
+                  @"Depth":@"47.80",
+                  @"NST":@"7",
+                  @"Region":@"Kenai Peninsula, Alaska"
+                  };
+
+                exampleDictionary2 =
+                @{
+                  @"Src":@"nc",
+                  @"Eqid":@"71958305",
+                  @"Version":@"0",
+                  @"Datetime":@"Friday, March 22, 2013 04:24:39 UTC",
+                  @"Lat":@"35.5412",
+                  @"Lon":@"-120.7735",
+                  @"Magnitude":@"1.2",
+                  @"Depth":@"5.10",
+                  @"NST":@"10",
+                  @"Region":@"Central California"
+                  };
+
+                exampleUSGSData = [exampleUSGSDataString dataUsingEncoding:NSUTF8StringEncoding];
+                loadedData = nil;
+                dummyLoadDataBlock = ^(NSArray *data) {
+                    loadedData = data;
+                };
+                successBlock = [earthquakeDataService buildSuccessBlockWithLoadDataBlock:dummyLoadDataBlock];
+                successBlock(nil,exampleUSGSData);
+            });
+
+            it(@"should create array of dictionaries with earthquake data built from network request results", ^{
+                [[loadedData should] haveCountOf:2];
+                for (id item in loadedData) {
+                    [[item should] beKindOfClass:[NSDictionary class]];
+                }
+            });
+
+            it(@"first item should match exampleDictionary1", ^{
+                NSDictionary *actionDictionary1 = [loadedData objectAtIndex:0];
+                [[actionDictionary1 should] equal:exampleDictionary1];
+            });
+
+            it(@"second item should match exampleDictionary2", ^{
+                NSDictionary *actionDictionary2 = [loadedData objectAtIndex:1];
+                [[actionDictionary2 should] equal:exampleDictionary2];
+            });
         });
     });
     
