c8c04e7e5c12818a9cf6f324e968daf69204244f
Douglas Sjoquist <dwsjoquist@sunetos.com>
2013-03-27 22:51:58 -0400

    add ShapeUpIntegrationSpecs target with failing test to ensure target is working

---
+++ ShakeUpIntegrationSpecs/IVGEarthquakeDataServiceIntegrationSpecs.m
@@ -0,0 +1,24 @@
+//
+//  IVGEarthquakeDataServiceIntegrationSpecs.m
+//  ShakeUp
+//
+//  Created by Douglas Sjoquist on 03/27/13.
+//  Copyright (c) 2012 Ivy Gulch, LLC. All rights reserved.
+//
+
+#import "Kiwi.h"
+#import "AFNetworking.h"
+#import "IVGEarthquakeDataService.h"
+#import "IVGUSGSAPIConstants.h"
+#import "IVGEarthquake.h"
+
+SPEC_BEGIN(IVGEarthquakeDataServiceIntegrationSpecs)
+
+describe(@"earthquakeDataService integration", ^{
+
+    it(@"test", ^{
+        [@"fail" shouldBeNil];
+    });
+});
+
+SPEC_END
